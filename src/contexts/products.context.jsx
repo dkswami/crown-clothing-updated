065f5dd0bfa6029c8eda2be5fd4bd67bf0c46ad4
dkswami <dkswami1820@gmail.com>
2022-08-08 14:17:02 +0530

    14: cartitem functionality added

---
+++ src/contexts/products.context.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState, useEffect, useContext } from "react";
+import PRODUCTS from '../shop-data.json';
+
+export const ProductsContext = createContext({
+	products: [],
+});
+
+export const ProductsProvider = ({ children }) => {
+	const [ products, useProducts ] = useState(PRODUCTS);
+	const value = {products}
+	return(
+		<ProductsContext.Provider value={value}>
+			{ children }
+		</ProductsContext.Provider>
+	)
+}
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,82 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth,
+	signInWithPopup,
+	GoogleAuthProvider,
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+	signOut,
+	onAuthStateChanged,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCpAoGIQpY8uxRd1iBQmj8uOM1LiLLyQaQ",
+  authDomain: "crown-clothing-db-87ce8.firebaseapp.com",
+  projectId: "crown-clothing-db-87ce8",
+  storageBucket: "crown-clothing-db-87ce8.appspot.com",
+  messagingSenderId: "884964496482",
+  appId: "1:884964496482:web:925a79e8aaa832e900ed3b",
+  measurementId: "G-T5F07Y6RBS"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
+
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
+	prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+	signInWithPopup(auth, googleProvider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async(userAuth, additionalInformation = {}) => {
+	if(!userAuth) return;
+	
+	const userDocRef = doc(db, 'users', userAuth.uid);
+	const userSnapshot = await getDoc(userDocRef);
+
+	if(!userSnapshot.exists()) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await setDoc(userDocRef, { 
+				displayName, email, createdAt, ...additionalInformation,});
+
+		} catch(error) {
+			console.log('error creating the user', error.message);
+		}
+	}
+
+	return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async(email, password) => {
+	if(!email || !password) return;
+
+	return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async(email, password) => {
+	if(!email || !password) return;
+
+	return await signInWithEmailAndPassword(auth, email, password);
+};
+
+export const signOutUser = async() =>  await signOut(auth);
+
+export const onAuthStateChangedListener = (callback) => {
+	onAuthStateChanged(auth,callback);
+}
