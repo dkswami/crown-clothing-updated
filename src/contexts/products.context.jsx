d8bd7507addd8547567b4beb5f5e07667b37ea39
dkswami <dkswami1820@gmail.com>
2022-08-13 15:57:27 +0530

    shop data added and add collection and documents code included in firebase utils

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,12 +1,18 @@
 import { createContext, useState, useEffect, useContext } from "react";
-import PRODUCTS from '../shop-data.json';
+import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
+import SHOP_DATA from '../shop-data.js';
 
 export const ProductsContext = createContext({
 	products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-	const [ products, useProducts ] = useState(PRODUCTS);
+	const [ products, setProducts ] = useState(SHOP_DATA);
+
+	// useEffect(() => {
+	// 	addCollectionAndDocuments('collections',SHOP_DATA);
+	// },[]);
+
 	const value = {products}
 	return(
 		<ProductsContext.Provider value={value}>
