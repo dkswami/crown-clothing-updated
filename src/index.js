cd361f7099f8396cb25a7cd17ba2e655e6303bdc
dkswami <dkswami1820@gmail.com>
2022-08-14 13:44:05 +0530

    20: category preview component added and 4 product data displayed in shop page

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { UserProvider } from './contexts/user.context';
-import { CategoriesProvider } from './contexts/products.context';
+import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 
 
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,24 +1,16 @@
-import { useContext, Fragment } from 'react';
+import { useContext } from 'react';
 import './shop.styles.scss';
-import { CategoriesContext } from '../../contexts/products.context';
-import ProductCard from '../../components/product-card/product-card.component';
-
+import { CategoriesContext } from '../../contexts/categories.context';
+import CategoryPreview from '../../components/category-preview/category-preview.component';
 const Shop = () => {
 	const { categoriesMap } = useContext(CategoriesContext);
-
 	return(
-		<Fragment>
-			{Object.keys(categoriesMap).map((title) => (
-				<Fragment key={title}>
-					<h2>{title}</h2>
-					<div className='products-container'>
-						{categoriesMap[title].map((product) => (
-							<ProductCard key={product.id} product={product} />
-						))}
-					</div>
-				</Fragment>
-			))}
-		</Fragment>		
+		<div className='shop-container'>
+			{Object.keys(categoriesMap).map((key) => {
+				const products = categoriesMap[key];
+				return <CategoryPreview key={key} title={key} products={products} />;
+			})}
+		</div>	
 	);
 }
 
