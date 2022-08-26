a311df587b1ca7bca953f43104bf8bd9a9a533b2
dkswami <dkswami1820@gmail.com>
2022-08-27 00:51:04 +0530

    added redirect and minor changes

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,5 @@
 import { Fragment, useContext } from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import { UserContext } from '../../contexts/user.context';
 import { CartContext } from "../../contexts/cart.context";
@@ -21,7 +21,6 @@ const Navigation = () => {
 	const signOutHandler = async () => {
 		await signOutUser();
 		setCurrentUser(null);
-		console.log(currentUser);
 	}
 
 	return(
