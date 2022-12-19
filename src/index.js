cf35e3c1b68a849b7a61cffff309515500b0d93b
dkswami <dkswami1820@gmail.com>
2022-12-19 18:12:32 +0530

    before laptop change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import './index.scss';
 import App from './App';
@@ -8,20 +9,22 @@ import reportWebVitals from './reportWebVitals';
 import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
-
+import { store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<UserProvider>
-				<CategoriesProvider>
-					<CartProvider>
-						<App />
-					</CartProvider>
-				</CategoriesProvider>
-			</UserProvider>		
-		</BrowserRouter>
+		<Provider store={store}>
+			<BrowserRouter>
+				<UserProvider>
+					<CategoriesProvider>
+						<CartProvider>
+							<App />
+						</CartProvider>
+					</CategoriesProvider>
+				</UserProvider>		
+			</BrowserRouter>
+		</Provider>		
 	</React.StrictMode>
   
 );
